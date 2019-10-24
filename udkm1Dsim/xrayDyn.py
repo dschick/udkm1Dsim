91ec77f89d5322e65b1f973e9d7da4756ccaa0f5
Daniel Schick <schick.daniel@gmail.com>
2019-10-24 23:26:39 +0200

    inital submit xrayDyn

---
+++ udkm1Dsim/xrayDyn.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the udkm1Dsimpy module.
+#
+# udkm1Dsimpy is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2017 Daniel Schick
+
+"""A :mod:`XrayDyn` module """
+
+__all__ = ["XrayDyn"]
+
+__docformat__ = "restructuredtext"
+
+import numpy as np
+from .xray import Xray
+from .unitCell import UnitCell
+from . import u
+from time import time
+
+
+class XrayDyn(Xray):
+    """XrayDyn
+
+    Dynamical Xray simulations
+
+    Args:
+        S (object): sample to do simulations with
+        force_recalc (boolean): force recalculation of results
+
+    Attributes:
+        S (object): sample to do simulations with
+        force_recalc (boolean): force recalculation of results
+        polarization (float): polarization state
+
+    """
+
+    def __init__(self, S, force_recalc, **kwargs):
+        super().__init__(S, force_recalc, **kwargs)
+
+    def __str__(self):
+        """String representation of this class"""
+        class_str = 'Kinematical X-Ray Diffraction simulation properties:\n\n'
+        class_str += super().__str__()
+        return class_str
