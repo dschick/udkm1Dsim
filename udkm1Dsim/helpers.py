ffbaaecde4d3e95c70ea2710cbefaa873afd2a60
Daniel Schick <schick.daniel@gmail.com>
2019-10-25 10:40:41 +0200

    add helper module
    first helper is for hashing dicts

---
+++ udkm1Dsim/helpers.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the udkm1Dsimpy module.
+#
+# udkm1Dsimpy is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2017 Daniel Schick
+
+"""A :mod:`helpers` module """
+
+__all__ = ['make_hash_sha256', 'make_hashable']
+
+__docformat__ = "restructuredtext"
+
+import hashlib
+import base64
+
+
+def make_hash_sha256(o):
+    hasher = hashlib.sha256()
+    hasher.update(repr(make_hashable(o)).encode())
+    return base64.b64encode(hasher.digest()).decode()
+
+
+def make_hashable(o):
+    if isinstance(o, (tuple, list)):
+        return tuple((make_hashable(e) for e in o))
+
+    if isinstance(o, dict):
+        return tuple(sorted((k, make_hashable(v)) for k, v in o.items()))
+
+    if isinstance(o, (set, frozenset)):
+        return tuple(sorted(make_hashable(e) for e in o))
+
+    return o
