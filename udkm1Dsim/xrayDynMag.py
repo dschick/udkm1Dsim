4d7a2e626fc8297b9dd246e647b2d81ced2bf779
Daniel Schick <schick.daniel@gmail.com>
2019-11-19 09:25:11 +0100

    get rid of for loops

--- udkm1Dsim/xrayDynMag.py
+++ udkm1Dsim/xrayDynMag.py
@@ -130,103 +130,106 @@ class XrayDynMag(Xray):
              np.sin(mag_phi) * np.sin(mag_gamma),
              np.cos(mag_phi)]
 
-        eps = np.zeros([M, 3, 3], dtype=np.cfloat)
+        eps = np.zeros([M, N, 3, 3], dtype=np.cfloat)
 #        k_z = np.zeros([M, N], dtype=np.cfloat)
 
         A = np.zeros([M, N, 4, 4], dtype=np.cfloat)
         P = np.zeros([M, N, 4, 4], dtype=np.cfloat)
 
-        for k in range(M):
-            energy = self._energy[k]
-            factor = 830.94/energy**2
-            try:
-                if atom.id == 'Fe':
-                    density = 0.142
-                elif atom.id == 'Cr':
-                    density = 0.1348
-                else:
-                    density = 0.069
-            except AttributeError:
-                density = 0
-#            qz = self._qz[k, :]
-            theta = self._theta[k, :]
-            try:
-                cf = atom.get_atomic_form_factor(energy)
-            except AttributeError:
-                cf = 0
+        try:
+            if atom.id == 'Fe':
+                density = 0.142
+            elif atom.id == 'Cr':
+                density = 0.1348
+            else:
+                density = 0.069
+        except AttributeError:
+            density = 0
+
+        energy = self._energy
+        factor = 830.94/energy**2
+        theta = self._theta
+
+        try:
+            cf = atom.get_atomic_form_factor(energy)
+        except AttributeError:
+            cf = 0
 #            mag = mag_amplitude * atom.get_magnetic_scattering_factor(energy)
-            mag = 0 * factor * mag_amplitude * 0.01 * cf
+        mag = 0 * factor * mag_amplitude * 0.01 * cf
+        mag = np.tile(mag[:, np.newaxis], [1, N])
 
-            eps0 = 1 - factor*density*cf
-            eps[k, 0, 0] = eps0
-            eps[k, 0, 1] = -1j * u[2] * mag
-            eps[k, 0, 2] = 1j * u[1] * mag
-            eps[k, 1, 0] = -eps[k, 0, 1]
-            eps[k, 1, 1] = eps0
-            eps[k, 1, 2] = -1j * u[0] * mag
-            eps[k, 2, 0] = -eps[k, 0, 2]
-            eps[k, 2, 1] = -eps[k, 1, 2]
-            eps[k, 2, 2] = eps0
+        eps0 = 1 - factor*density*cf
+        eps0 = np.tile(eps0[:, np.newaxis], [1, N])
 
-            alpha_y = np.cos(theta) / np.sqrt(eps[k, 0, 0])
-            alpha_z = np.sqrt(1 - alpha_y**2)
+        eps[:, :, 0, 0] = eps0
+        eps[:, :, 0, 1] = -1j * u[2] * mag
+        eps[:, :, 0, 2] = 1j * u[1] * mag
+        eps[:, :, 1, 0] = -eps[:, :, 0, 1]
+        eps[:, :, 1, 1] = eps0
+        eps[:, :, 1, 2] = -1j * u[0] * mag
+        eps[:, :, 2, 0] = -eps[:, :, 0, 2]
+        eps[:, :, 2, 1] = -eps[:, :, 1, 2]
+        eps[:, :, 2, 2] = eps0
 
-            # k_z[k, :] = self._k[k] * np.sqrt(eps[k, 0, 0]) * alpha_z
+        alpha_y = np.divide(np.cos(theta), np.sqrt(eps[:, :, 0, 0]))
+        alpha_z = np.sqrt(1 - alpha_y**2)
 
-            n_right_down = np.sqrt(eps[k, 0, 0] - 1j * eps[k, 0, 2] * alpha_y
-                                   - 1j * eps[k, 0, 1] * alpha_z)
-            n_left_down = np.sqrt(eps[k, 0, 0] + 1j * eps[k, 0, 2] * alpha_y
-                                  + 1j * eps[k, 0, 1] * alpha_z)
-            n_right_up = np.sqrt(eps[k, 0, 0] - 1j * eps[k, 0, 2] * alpha_y
-                                 + 1j * eps[k, 0, 1] * alpha_z)
-            n_left_up = np.sqrt(eps[k, 0, 0] + 1j * eps[k, 0, 2] * alpha_y
-                                - 1j * eps[k, 0, 1] * alpha_z)
+        n_right_down = np.sqrt(eps[:, :, 0, 0] - 1j * eps[:, :, 0, 2] * alpha_y
+                               - 1j * eps[:, :, 0, 1] * alpha_z)
+        n_left_down = np.sqrt(eps[:, :, 0, 0] + 1j * eps[:, :, 0, 2] * alpha_y
+                              + 1j * eps[:, :, 0, 1] * alpha_z)
+        n_right_up = np.sqrt(eps[:, :, 0, 0] - 1j * eps[:, :, 0, 2] * alpha_y
+                             + 1j * eps[:, :, 0, 1] * alpha_z)
+        n_left_up = np.sqrt(eps[:, :, 0, 0] + 1j * eps[:, :, 0, 2] * alpha_y
+                            - 1j * eps[:, :, 0, 1] * alpha_z)
 
-            alpha_y_right_down = np.cos(theta)/n_right_down
-            alpha_z_right_down = np.sqrt(1-alpha_y_right_down**2)
-            alpha_y_left_down = np.cos(theta)/n_left_down
-            alpha_z_left_down = np.sqrt(1-alpha_y_left_down**2)
-            alpha_y_right_up = np.cos(theta)/n_right_up
-            alpha_z_right_up = np.sqrt(1-alpha_y_right_up**2)
-            alpha_y_left_up = np.cos(theta)/n_left_up
-            alpha_z_left_up = np.sqrt(1-alpha_y_left_up**2)
+        alpha_y_right_down = np.cos(theta)/n_right_down
+        alpha_z_right_down = np.sqrt(1-alpha_y_right_down**2)
+        alpha_y_left_down = np.cos(theta)/n_left_down
+        alpha_z_left_down = np.sqrt(1-alpha_y_left_down**2)
+        alpha_y_right_up = np.cos(theta)/n_right_up
+        alpha_z_right_up = np.sqrt(1-alpha_y_right_up**2)
+        alpha_y_left_up = np.cos(theta)/n_left_up
+        alpha_z_left_up = np.sqrt(1-alpha_y_left_up**2)
 
-            A[k, :, 0, 0] = -1 - 1j * eps[k, 0, 1] * alpha_z_right_down
-            - 1j * eps[k, 0, 2] * alpha_y_right_down
-            A[k, :, 0, 1] = 1 - 1j * eps[k, 0, 1] * alpha_z_left_down
-            - 1j * eps[k, 0, 2] * alpha_y_left_down
-            A[k, :, 0, 2] = -1 + 1j * eps[k, 0, 1] * alpha_z_right_up
-            - 1j * eps[k, 0, 2] * alpha_y_right_up
-            A[k, :, 0, 3] = 1 + 1j * eps[k, 0, 1] * alpha_z_left_up
-            - 1j * eps[k, 0, 2] * alpha_y_left_up
+        A[:, :, 0, 0] = -1 - 1j * eps[:, :, 0, 1] * alpha_z_right_down
+        - 1j * eps[:, :, 0, 2] * alpha_y_right_down
+        A[:, :, 0, 1] = 1 - 1j * eps[:, :, 0, 1] * alpha_z_left_down
+        - 1j * eps[:, :, 0, 2] * alpha_y_left_down
+        A[:, :, 0, 2] = -1 + 1j * eps[:, :, 0, 1] * alpha_z_right_up
+        - 1j * eps[:, :, 0, 2] * alpha_y_right_up
+        A[:, :, 0, 3] = 1 + 1j * eps[:, :, 0, 1] * alpha_z_left_up
+        - 1j * eps[:, :, 0, 2] * alpha_y_left_up
 
-            A[k, :, 1, 0] = 1j * alpha_z_right_down - eps[k, 0, 1]
-            - 1j * eps[k, 1, 2] * alpha_y_right_down
-            A[k, :, 1, 1] = 1j * alpha_z_left_down + eps[k, 0, 1]
-            - 1j * eps[k, 1, 2] * alpha_y_left_down
-            A[k, :, 1, 2] = -1j * alpha_z_right_up - eps[k, 0, 1]
-            - 1j * eps[k, 1, 2] * alpha_y_right_up
-            A[k, :, 1, 3] = -1j * alpha_z_left_up + eps[k, 0, 1]
-            - 1j * eps[k, 1, 2] * alpha_y_left_up
+        A[:, :, 1, 0] = 1j * alpha_z_right_down - eps[:, :, 0, 1]
+        - 1j * eps[:, :, 1, 2] * alpha_y_right_down
+        A[:, :, 1, 1] = 1j * alpha_z_left_down + eps[:, :, 0, 1]
+        - 1j * eps[:, :, 1, 2] * alpha_y_left_down
+        A[:, :, 1, 2] = -1j * alpha_z_right_up - eps[:, :, 0, 1]
+        - 1j * eps[:, :, 1, 2] * alpha_y_right_up
+        A[:, :, 1, 3] = -1j * alpha_z_left_up + eps[:, :, 0, 1]
+        - 1j * eps[:, :, 1, 2] * alpha_y_left_up
 
-            A[k, :, 2, 0] = -1j * n_right_down * A[k, :, 0, 0]
-            A[k, :, 2, 1] = 1j * n_left_down * A[k, :, 0, 1]
-            A[k, :, 2, 2] = -1j * n_right_up * A[k, :, 0, 2]
-            A[k, :, 2, 3] = 1j * n_left_up * A[k, :, 0, 3]
+        A[:, :, 2, 0] = -1j * n_right_down * A[:, :, 0, 0]
+        A[:, :, 2, 1] = 1j * n_left_down * A[:, :, 0, 1]
+        A[:, :, 2, 2] = -1j * n_right_up * A[:, :, 0, 2]
+        A[:, :, 2, 3] = 1j * n_left_up * A[:, :, 0, 3]
 
-            A[k, :, 3, 0] = - alpha_z_right_down * n_right_down * A[k, :, 0, 0]
-            A[k, :, 3, 1] = - alpha_z_left_down * n_left_down * A[k, :, 0, 1]
-            A[k, :, 3, 2] = alpha_z_right_up * n_right_up * A[k, :, 0, 2]
-            A[k, :, 3, 3] = alpha_z_left_up * n_left_up * A[k, :, 0, 3]
+        A[:, :, 3, 0] = - alpha_z_right_down * n_right_down * A[:, :, 0, 0]
+        A[:, :, 3, 1] = - alpha_z_left_down * n_left_down * A[:, :, 0, 1]
+        A[:, :, 3, 2] = alpha_z_right_up * n_right_up * A[:, :, 0, 2]
+        A[:, :, 3, 3] = alpha_z_left_up * n_left_up * A[:, :, 0, 3]
 
-            A[k, :, :, :] = A[k, :, :, :] / (np.sqrt(2) * eps[k, 0, 0])
+        A[:, :, :, :] = np.divide(A[:, :, :, :],
+                                  np.sqrt(2) * eps[:, :, 0, 0][:, :, np.newaxis, np.newaxis])
 
-            phase = self._k[k] * distance
+        phase = self._k * distance
+        phase = phase[:, np.newaxis]
 
-            P[k, :, 0, 0] = np.exp(1j * phase * n_right_down * alpha_z_right_down)
-            P[k, :, 1, 1] = np.exp(1j * phase * n_left_down * alpha_z_left_down)
-            P[k, :, 2, 2] = np.exp(-1j * phase * n_right_up * alpha_z_right_up)
-            P[k, :, 3, 3] = np.exp(-1j * phase * n_left_up * alpha_z_left_up)
+        P[:, :, 0, 0] = np.exp(1j * phase * n_right_down * alpha_z_right_down)
+        P[:, :, 1, 1] = np.exp(1j * phase * n_left_down * alpha_z_left_down)
+        P[:, :, 2, 2] = np.exp(-1j * phase * n_right_up * alpha_z_right_up)
+        P[:, :, 3, 3] = np.exp(-1j * phase * n_left_up * alpha_z_left_up)
 
         return A, P
 
@@ -240,13 +243,13 @@ class XrayDynMag(Xray):
         S = np.tile(np.eye(4, 4, dtype=np.cfloat)[np.newaxis, np.newaxis, :, :], (M, N, 1, 1))
         F = np.zeros_like(S)
         Ref = np.tile(np.eye(2, 2, dtype=np.cfloat)[np.newaxis, np.newaxis, :, :], (M, N, 1, 1))
-        
+
         # traverse all unit cells in the sample structure
         index = 0
         # vacuum
         A, P = self.get_atom_ref_trans_matrix([], 0, 0)
         last_A = A
-        
+
         for i in trange(self.S.get_number_of_unit_cells()):
             uc = uc_handles[i]
             K = uc.num_atoms  # number of atoms
@@ -256,7 +259,9 @@ class XrayDynMag(Xray):
                 else:
                     del_dist = uc.atoms[j+1][1](strain)-uc.atoms[j][1](strain)
 
-                A, P = self.get_atom_ref_trans_matrix(uc.atoms[j][0], uc._area, del_dist*uc._c_axis)
+                A, P = self.get_atom_ref_trans_matrix(uc.atoms[j][0],
+                                                      uc._area,
+                                                      del_dist*uc._c_axis)
                 F = np.einsum("lmij,lmjk->lmik", np.linalg.inv(A), last_A)
                 # skip roughness for now
                 # how about debye waller?
@@ -268,19 +273,18 @@ class XrayDynMag(Xray):
         d = np.divide(1, S[:, :, 3, 3] * S[:, :, 2, 2] - S[:, :, 3, 2] * S[:, :, 2, 3])
         Ref[:, :, 0, 0] = (-S[:, :, 3, 3] * S[:, :, 2, 0] + S[:, :, 2, 3] * S[:, :, 3, 0]) * d
         Ref[:, :, 0, 1] = (-S[:, :, 3, 3] * S[:, :, 2, 1] + S[:, :, 2, 3] * S[:, :, 3, 1]) * d
-        Ref[:, :, 1, 0] = ( S[:, :, 3, 2] * S[:, :, 2, 0] - S[:, :, 2, 2] * S[:, :, 3, 0]) * d
-        Ref[:, :, 1, 1] = ( S[:, :, 3, 2] * S[:, :, 2, 1] - S[:, :, 2, 2] * S[:, :, 3, 1]) * d
-        
-#        temp = np.tile(np.array([[-1, 1], [-1j, -1j]])[np.newaxis, np.newaxis, :, :], (M, N, 1, 1))
+        Ref[:, :, 1, 0] = (S[:, :, 3, 2] * S[:, :, 2, 0] - S[:, :, 2, 2] * S[:, :, 3, 0]) * d
+        Ref[:, :, 1, 1] = (S[:, :, 3, 2] * S[:, :, 2, 1] - S[:, :, 2, 2] * S[:, :, 3, 1]) * d
+
         temp = np.array([[-1, 1], [-1j, -1j]])
         Ref = np.matmul(np.matmul(temp, Ref), temp/2)
         return Ref
 
     def get_reflectivity(self):
         Ref = self.calc_reflectivity()
-        Pol_in  = np.array([1+0.j, 0.j], dtype=complex) # to put "sigma" so as to update experimental panel easily.
+        Pol_in = np.array([1+0.j, 0.j], dtype=complex)
         Pol_out = np.array([1+0.j, 1+0.j], dtype=complex)
-        
+
         X = np.matmul(Ref, Pol_in)
-        R = np.real(np.matmul(np.square(np.absolute(X)),Pol_out))
+        R = np.real(np.matmul(np.square(np.absolute(X)), Pol_out))
         return R
