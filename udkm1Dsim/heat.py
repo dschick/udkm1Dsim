correct pulse energy calculation in heat

udkm1Dsim/heat.py
@@ -606,7 +606,8 @@ class Heat(Simulation):
         int_heat_capacities = self.S.get_layer_property_vector('_int_heat_capacity')
         thicknesses = self.S.get_layer_property_vector('_thickness')
         masses = self.S.get_layer_property_vector('_mass_unit_area')
-        E0 = np.array(fluence)*(1*u.angstrom**2)  # mass are normalized to 1Ang^2
+        # masses are normalized to 1Ang^2
+        E0 = np.array(fluence)*(1*u.angstrom**2).to_base_units().magnitude
 
         # check the intial temperature
         init_temp = self.check_initial_temperature(init_temp, distances)