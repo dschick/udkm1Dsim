92eac33ae68181a0f9fc2ec07fcbc8675413ddb4
Daniel Schick <schick.daniel@gmail.com>
2020-10-28 08:37:00 +0100

    finish helpers

--- udkm1Dsim/helpers.py
+++ udkm1Dsim/helpers.py
@@ -22,38 +22,69 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
-"""A :mod:`helpers` module """
-
 __all__ = ['make_hash_md5', 'make_hashable', 'm_power_x',
            'm_times_n', 'finderb', 'multi_gauss']
 
-__docformat__ = "restructuredtext"
+__docformat__ = 'restructuredtext'
 
 import hashlib
 import numpy as np
 
 
-def make_hash_md5(o):
+def make_hash_md5(obj):
+    """make_hash_md5
+
+
+    Args:
+        obj (any): anything that can be hashed.
+
+    Returns:
+        hash (str): hash from object.
+
+    """
     hasher = hashlib.md5()
-    hasher.update(repr(make_hashable(o)).encode())
+    hasher.update(repr(make_hashable(obj)).encode())
     return hasher.hexdigest()
 
 
-def make_hashable(o):
-    if isinstance(o, (tuple, list)):
-        return tuple((make_hashable(e) for e in o))
+def make_hashable(obj):
+    """make_hashable
 
-    if isinstance(o, dict):
-        return tuple(sorted((k, make_hashable(v)) for k, v in o.items()))
+    Recursive calls to elements of tuples, lists, dicts, set, and frozensets.
 
-    if isinstance(o, (set, frozenset)):
-        return tuple(sorted(make_hashable(e) for e in o))
+    Args:
+        obj (any): anything that can be hashed..
 
-    return o
+    Returns:
+        obj (tuple): hashable object.
+
+    """
+    if isinstance(obj, (tuple, list)):
+        return tuple((make_hashable(e) for e in obj))
+
+    if isinstance(obj, dict):
+        return tuple(sorted((k, make_hashable(v)) for k, v in obj.items()))
+
+    if isinstance(obj, (set, frozenset)):
+        return tuple(sorted(make_hashable(e) for e in obj))
+
+    return obj
 
 
 def m_power_x(m, x):
-    # apply exponent to each matrix in a numpy array
+    """m_power_x
+
+    Apply ``numpy.linalg.matrix_power`` to last 2 dimensions of 4-dimensional
+    input matrix.
+
+    Args:
+        m (ndarray[float, complex]): 4-dimensional input matrix.
+        x (float): exponent.
+
+    Returns:
+        m (ndarray[float, complex]): resulting matrix.
+
+    """
     if x > 1:
         for i in range(np.size(m, 0)):
             for j in range(np.size(m, 1)):
@@ -62,19 +93,38 @@ def m_power_x(m, x):
 
 
 def m_times_n(m, n):
+    """m_times_n
+
+    Matrix multiplication of last 2 dimensions for two 4-dimensional input
+    matrices.
+
+    Args:
+        m (ndarray[float, complex]): 4-dimensional input matrix.
+        n (ndarray[float, complex]): 4-dimensional input matrix.
+
+    Returns:
+        res (ndarray[float, complex]): 4-dimensional multiplication result.
+
+    """
     return np.einsum("lmij,lmjk->lmik", m, n)
 
 
-def finderb(key, vector):
+def finderb(key, array):
     """finderb
 
-    Binary search algorithm for sorted vector. Searches for the first
-    index ``i`` of vector where ``key`` >= ``vector[i]``.
-    ``key`` can be a scalar or a np.ndarray of keys.
-    ``vector`` must be a sorted np.ndarray
+    Binary search algorithm for sorted array. Searches for the first index
+    ``i`` of array where ``key`` >= ``array[i]``. ``key`` can be a scalar or
+    a np.ndarray of keys. ``array`` must be a sorted np.ndarray.
 
-    author: André Bojahr
-    licence: BSD
+    Author: André Bojahr.
+    Licence: BSD.
+
+    Args:
+        key (float, ndarray[float]): single or multiple sorted keys.
+        array (ndarray[float]): sorted array.
+
+    Returns:
+        i (ndarray[float]): position indices for each key in the array.
 
     """
     key = np.array(key, ndmin=1)
@@ -82,27 +132,37 @@ def finderb(key, vector):
     i = np.zeros([n], dtype=int)
 
     for m in range(n):
-        i[m] = finderb_nest(key[m], vector)
+        i[m] = finderb_nest(key[m], array)
     return i
 
 
-def finderb_nest(key, vector):
+def finderb_nest(key, array):
     """finderb_nest
 
-    nested sub-function of finderb
+    Nested sub-function of :func:`.finderb` for one single key.
+
+    Author: André Bojahr.
+    Licence: BSD.
+
+    Args:
+        key (float): single key.
+        array (ndarray[float]): sorted array.
+
+    Returns:
+        a (float): position index of key in the array.
 
     """
     a = 0  # start of intervall
-    b = len(vector)  # end of intervall
+    b = len(array)  # end of intervall
 
     # if the key is smaller than the first element of the
     # vector we return 1
-    if key < vector[0]:
+    if key < array[0]:
         return 0
 
     while (b-a) > 1:  # loop until the intervall is larger than 1
         c = int(np.floor((a+b)/2))  # center of intervall
-        if key < vector[c]:
+        if key < array[c]:
             # the key is in the left half-intervall
             b = c
         else:
@@ -112,11 +172,20 @@ def finderb_nest(key, vector):
     return a
 
 
-def multi_gauss(x, s=1, x0=0, A=1):
+def multi_gauss(x, s=[1], x0=[0], A=[1]):
     """multi_gauss
 
-    multiple gauss functions with width given as FWHM and area normalized
-    to one and maximum of gauss at x0.
+    Multiple gauss functions with width ``s`` given as FWHM and area normalized
+    to input ``A`` and maximum of gauss at ``x0``.
+
+    Args:
+        x (ndarray[float]): argument of multi_gauss.
+        s (ndarray[float], optional): FWHM of Gaussians. Defaults to 1.
+        x0 (ndarray[float], optional): centers of Gaussians. Defaults to 0.
+        A (ndarray[float], optional): amplitudes of Gaussians. Defaults to 1.
+
+    Returns:
+        y (ndarray[float]): multiple Gaussians.
 
     """
     s = np.asarray(s)/(2*np.sqrt(2*np.log(2)))
