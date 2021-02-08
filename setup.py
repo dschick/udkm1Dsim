bb99618f8baf0ab1e3c7022b0b379ec5dadfd13b
Daniel Schick <schick.daniel@gmail.com>
2021-02-08 12:57:57 +0100

    update version to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='udkm1Dsim',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     package_data={
         'udkm1Dsim': ['parameters/atomic_form_factors/chantler/*.cf',
