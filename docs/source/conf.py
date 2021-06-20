02f83f8bfb47a5200f5fde45c93038fff8f1e09e
Daniel Schick <schick.daniel@gmail.com>
2021-06-20 23:25:18 +0200

    Bump version: 1.4.3 → 1.4.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Daniel Schick'
 author = 'Daniel Schick'
 
 # The short X.Y version
-version = '1.4.3'
+version = '1.4.4'
 # The full version, including alpha/beta/rc tags
 release = version
 
