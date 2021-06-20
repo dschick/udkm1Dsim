9a8f4ad35a0a68c5eeb3003c39e06b946b231bfa
Daniel Schick <schick.daniel@gmail.com>
2021-06-20 23:26:47 +0200

    Bump version: 1.4.4 → 1.4.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='udkm1Dsim',
-    version='1.4.4',
+    version='1.4.5',
     packages=find_packages(),
     package_data={
         'udkm1Dsim': ['parameters/atomic_form_factors/chantler/*.cf',
@@ -19,7 +19,7 @@ setup(
     install_requires=['tqdm>=4.43.0',
                       'numpy>=1.18.2',
                       'pint>=0.9',
-                      'scipy>=1.4.4',
+                      'scipy>=1.4.5',
                       'sympy>=1.5.1',
                       'tabulate',
                       'matplotlib>=2.0.0'],
