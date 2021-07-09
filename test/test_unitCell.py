10f971427b8d4ab0a89ac49a8d3c2369ae874499
Daniel Schick <schick@mbi-berlin.de>
2021-07-09 22:55:23 +0200

    update test for unit cell T-dependent parameters

--- test/test_unitCell.py
+++ test/test_unitCell.py
@@ -27,3 +27,41 @@ def test_unit_cell():
     assert uc.therm_cond[0](300) == 1
     assert uc.opt_pen_depth == 11*u.nm
     assert uc.sound_vel == 5*(u.nm/u.ps)
+    # test temperature-dependent parameters for float input
+    uc.heat_capacity = [10, 1000]
+    uc.therm_cond = [10, 1000]
+    uc.lin_therm_exp = [10, 1000]
+    assert uc.heat_capacity_str == ['10', '1000']
+    assert uc.heat_capacity[0](300) == 10
+    assert uc.heat_capacity[1](300) == 1000
+    assert uc.int_heat_capacity_str == ['10*T', '1000*T']
+    assert uc.int_heat_capacity[0](300) == 3000
+    assert uc.int_heat_capacity[1](300) == 300000
+    assert uc.therm_cond_str == ['10', '1000']
+    assert uc.therm_cond[0](300) == 10
+    assert uc.therm_cond[1](300) == 1000
+    assert uc.lin_therm_exp_str == ['10', '1000']
+    assert uc.lin_therm_exp[0](300) == 10
+    assert uc.lin_therm_exp[1](300) == 1000
+    assert uc.int_lin_therm_exp_str == ['10*T', '1000*T']
+    assert uc.int_lin_therm_exp[0](300) == 3000
+    assert uc.int_lin_therm_exp[1](300) == 300000
+    # test temperature-dependent parameters for str function input
+    uc.heat_capacity = ['10*T', 'exp(300-T)+300']
+    uc.therm_cond = ['10*T', 'exp(300-T)+300']
+    uc.lin_therm_exp = ['10*T', 'exp(300-T)+300']
+    assert uc.heat_capacity_str == ['10*T', 'exp(300-T)+300']
+    assert uc.heat_capacity[0](300) == 3000
+    assert uc.heat_capacity[1](300) == 301
+    assert uc.int_heat_capacity_str == ['5*T**2', '300*T - exp(300 - T)']
+    assert uc.int_heat_capacity[0](300) == 450000
+    assert uc.int_heat_capacity[1](300) == 89999.0
+    assert uc.therm_cond_str == ['10*T', 'exp(300-T)+300']
+    assert uc.therm_cond[0](300) == 3000
+    assert uc.therm_cond[1](300) == 301
+    assert uc.lin_therm_exp_str == ['10*T', 'exp(300-T)+300']
+    assert uc.lin_therm_exp[0](300) == 3000
+    assert uc.lin_therm_exp[1](300) == 301
+    assert uc.int_lin_therm_exp_str == ['5*T**2', '300*T - exp(300 - T)']
+    assert uc.int_lin_therm_exp[0](300) == 450000
+    assert uc.int_lin_therm_exp[1](300) == 89999.0
