cfc7b64429781e8eec46a549c6e9a2b6b5d36155
Daniel Schick <schick@mbi-berlin.de>
2021-07-09 23:11:45 +0200

    fix test for atom position

--- test/test_unitCell.py
+++ test/test_unitCell.py
@@ -12,8 +12,8 @@ def test_unit_cell():
     uc = UnitCell('uc', 'Unit Cell', 2.86*u.angstrom, heat_capacity=10*(u.J/u.kg/u.K),
                   lin_therm_exp=1e-6/u.K, therm_cond=1*(u.W/u.m/u.K),
                   opt_pen_depth=11*u.nm, sound_vel=5*(u.nm/u.ps))
-    uc.add_atom(Fe, 'lambda strain: 0*(strain+1)')
-    uc.add_atom(Fe, 'lambda strain: 0.5*(strain+1)')
+    uc.add_atom(Fe, '0*(s+1)')
+    uc.add_atom(Fe, '0.5*(s+1)')
 
     assert uc.id == 'uc'
     assert uc.name == 'Unit Cell'
