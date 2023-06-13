3b180d72e9ca2dc8f8141aa4de5356ba5ddfaf2a
Daniel Schick <schick@mbi-berlin.de>
2023-06-13 09:55:33 +0200

    fix autodoc discovery

--- udkm1Dsim/simulations/magnetization.py
+++ udkm1Dsim/simulations/magnetization.py
@@ -22,7 +22,7 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
-__all__ = ['Magnetization']
+__all__ = ['Magnetization', 'LLB']
 
 __docformat__ = 'restructuredtext'
 
