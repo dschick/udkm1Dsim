6f15add8a773b527848e4e1be7c1a8c3d42f1a22
Nilodirf <106656383+Nilodirf@users.noreply.github.com>
2023-06-24 13:31:02 +0200

    Update magnetization.py
    checked cartesian to polar conversion method with flake8

--- udkm1Dsim/simulations/magnetization.py
+++ udkm1Dsim/simulations/magnetization.py
@@ -1014,16 +1014,16 @@ class LLB(Magnetization):
         """
         polar = np.zeros_like(cartesian)
 
-        xs=cartesian[..., 0]
-        ys=cartesian[..., 1]
-        zs=cartesian[..., 2]
-        trans_amplitudes=np.hypot(xs, ys)
-        amplitudes=np.hypot(trans_amplitudes, zs)
-        zeromag_mask=amplitudes!=0.
-        polar[..., 0]=amplitudes   
-        polar[..., 1]=np.arctan2(ys, xs)               
-        polar[zeromag_mask, 2]=np.arccos(np.divide(zs,amplitudes))
-        
+        xs = cartesian[..., 0]
+        ys = cartesian[..., 1]
+        zs = cartesian[..., 2]
+        trans_amplitudes = np.hypot(xs, ys)
+        amplitudes = np.hypot(trans_amplitudes, zs)
+        zeromag_mask = amplitudes != 0.
+        polar[..., 0] = amplitudes
+        polar[..., 1] = np.arctan2(ys, xs)
+        polar[zeromag_mask, 2] = np.arccos(np.divide(zs, amplitudes))
+    
         return polar
 
     @property
