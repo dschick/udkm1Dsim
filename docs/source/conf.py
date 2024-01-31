93142cb5e33d1f672d8113289a55dc90aab4edfc
Daniel Schick <schick@mbi-berlin.de>
2024-01-31 08:54:34 +0100

    update sphinx related versions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'udkm1Dsim'
-copyright = '2022, Daniel Schick'
+copyright = '2024, Daniel Schick'
 author = 'Daniel Schick'
 
 # The short X.Y version
@@ -33,7 +33,7 @@ release = version
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '4.3'
+needs_sphinx = '5.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -49,6 +49,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx_copybutton',
     'sphinx_design',
+    'sphinx-book-theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -71,7 +72,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -124,6 +125,8 @@ html_theme_options = {
     'navigation_depth': 5,
     'collapse_navigation': False,
     'logo_only': True,
+    'repository_url': 'https://github.com/dschick/udkm1Dsim',
+    'use_repository_button': True,
 }
 
 html_logo = '_static/logo.png'
