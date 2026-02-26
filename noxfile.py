f6ac109fda1bb55ed1b4b267abbad1b142c1332a
Daniel Schick <schick.daniel@gmail.com>
2026-02-26 09:28:02 +0100

    introduce nox testing tool

---
+++ noxfile.py
@@ -0,0 +1,18 @@
+import nox
+
+
+python_versions = ["3.10", "3.11", "3.12", "3.13"]
+
+@nox.session(python=python_versions,
+             venv_backend="mamba")
+def lint(session):
+    session.install("flake8")
+    session.run("flake8", "./udkm1Dsim")
+
+
+@nox.session(python=python_versions,
+             venv_backend="mamba")
+def tests(session):
+    session.install("pytest-cov")
+    session.install(".")
+    session.run("pytest", "--cov=udkm1Dsim", "test/")
