43533037a93e0c1592d2e7500ebc57900a588423
Daniel Schick <schick.daniel@gmail.com>
2026-02-27 09:11:56 +0100

    fix flake8 starting in root project folder

--- noxfile.py
+++ noxfile.py
@@ -3,11 +3,12 @@ import nox
 
 python_versions = ["3.10", "3.11", "3.12", "3.13"]
 
+
 @nox.session(python=python_versions,
              venv_backend="mamba")
 def lint(session):
     session.install("flake8")
-    session.run("flake8", "./udkm1Dsim")
+    session.run("flake8", ".")
 
 
 @nox.session(python=python_versions,
