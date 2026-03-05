80f6319c19893b14796b079b8c335c4ff8b551fd
Daniel Schick <schick.daniel@gmail.com>
2026-03-05 18:11:17 +0100

    add tests for structure

--- test/test_structure.py
+++ test/test_structure.py
@@ -5,10 +5,9 @@ from udkm1Dsim import UnitCell, AmorphousLayer
 from udkm1Dsim import Structure
 from udkm1Dsim import u
 u.default_format = '~P'
-# import numpy as np
+import numpy as np
 import pytest
-# from pint.testing import assert_allclose as assert_approx
-# from pint.testing import assert_equal
+from pint.testing import assert_allclose
 
 
 # fixtures
@@ -53,54 +52,98 @@ def unit_cell_oxygen(atom_oxygen):
     return uc
 
 
-# structure amorph, crystalline, mixed, with and w/o substrate
-# I guess one could directly try with a mixed structure with substrate and sub_structure
 @pytest.fixture(scope='module')
-def structure_amorph(amorphous_layer_iron, amorphous_layer_oxygen):
+def structure(amorphous_layer_iron, amorphous_layer_oxygen, unit_cell_iron, unit_cell_oxygen):
     S = Structure('structure amorph')
     S.add_sub_structure(amorphous_layer_iron, 10)
     S.add_sub_structure(amorphous_layer_oxygen, 10)
+    S.add_sub_structure(unit_cell_iron, 20)
+
+    DL = Structure('double layer')
+    DL.add_sub_structure(unit_cell_iron, 5)
+    DL.add_sub_structure(unit_cell_oxygen, 7)
+
+    S.add_sub_structure(DL, 20)
+
+    substrate = Structure('substrate')
+    substrate.add_sub_structure(amorphous_layer_iron, 100)
+
+    S.add_substrate(substrate)
+
     return S
 
 
 # tests
 
-# __str__
 
-# visualize
-
-def test_structure_get_hash(structure_amorph):
-    structure_amorph.get_hash()
+def test_structure_str(structure):
+    structure.__str__()
 
 
-# add_sub_structure
+def test_structure_visualize(structure):
+    structure.visualize(block=False)
 
-# add_substrate
 
-# get_number_of_sub_structures
+def test_structure_get_hash(structure):
+    assert structure.get_hash(types='heat') == 'b3a2d778b80f935f6aeb70c0428f83a8'
+    assert structure.get_hash(types='phonon') == '7906ad75e7bd69cdf354ed3f7180d0f4'
+    assert structure.get_hash(types='xray') == 'ff455714885d2d028e8efa36ca391d64'
+    assert structure.get_hash(types='magnetic') == '5711dc2e9867eefa69c7fc230b216036'
 
-# get_number_of_layers
 
-# get_number_of_unique_layers
+def test_get_number_of_sub_structures(structure):
+    assert structure.get_number_of_sub_structures() == 5
 
-# get_thickness
 
-# get_unique_layers
+def test_get_number_of_layers(structure):
+    assert structure.get_number_of_layers() == 280
 
-# get_layer_vectors
 
-# get_all_positions_per_unique_layer
+def test_get_number_of_unique_layers(structure):
+    assert structure.get_number_of_unique_layers() == 4
 
-# get_distances_of_layers
 
-# get_distances_of_interfaces
+def test_get_thickness(structure):
+    assert_allclose(structure.get_thickness(), 150*u.nm, rtol=1e-2)
 
-# interp_distance_at_interfaces
 
-# get_layer_property_vector
+def test_get_unique_layers(structure):
+    layer_ids, _ = structure.get_unique_layers()
+    assert layer_ids == ['amorphous_layer_Fe', 'amorphous_layer_O', 'unit_cell_Fe', 'unit_cell_O']
 
-# get_layer_handle
 
-# reverse
+def test_get_layer_vectors(structure):
+    structure.get_layer_vectors()
 
-# reverse_sub_structures
+
+def test_get_all_positions_per_unique_layer(structure):
+    pos = structure.get_all_positions_per_unique_layer()
+    assert np.allclose(pos['amorphous_layer_Fe'], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+
+def test_get_distances_of_layers(structure):
+    d_start, _, _ = structure.get_distances_of_layers()
+    assert_allclose(np.sum(d_start), 2.22e-05*u.m, rtol=1e-2)
+
+
+def test_get_distances_of_interfaces(structure):
+    assert_allclose(np.sum(structure.get_distances_of_interfaces()), 3.68e-6*u.m, rtol=1e-2)
+
+
+def test_interp_distance_at_interfaces(structure):
+    structure.interp_distance_at_interfaces(N=11)
+
+
+def test_get_layer_property_vector(structure):
+    structure.get_layer_property_vector('density')
+    structure.get_layer_property_vector('_density')
+    structure.get_layer_property_vector('heat_capacity')
+    structure.get_layer_property_vector('spring_const')
+
+
+def test_get_layer_handle(structure):
+    structure.get_layer_handle(0)
+
+
+def test_reverse(structure):
+    structure.reverse()
--- udkm1Dsim/structures/structure.py
+++ udkm1Dsim/structures/structure.py
@@ -99,7 +99,8 @@ class Structure:
             class_str += tab_str + 'no substrate\n'
         return class_str
 
-    def visualize(self, block=True, unit='nm', fig_size=[20, 1], cmap='Set1', linewidth=0.1, show=True):
+    def visualize(self, block=True, unit='nm', fig_size=[20, 1], cmap='Set1', linewidth=0.1,
+                  show=True):
         """visualize
 
         Simple visualization of the structure.
