16333cdc98eaf1ab842bb3d2ebe7e6dc6cca4890
Daniel Schick <schick.daniel@gmail.com>
2026-03-05 23:29:59 +0100

    add test make_hash_md5

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# from udkm1Dsim.helpers import make_hash_md5, m_power_x, m_times_n #, multi_gauss
+from udkm1Dsim.helpers import make_hash_md5
+# , m_power_x, m_times_n
 from udkm1Dsim.helpers import finderb
+# , multi_gauss
 # from udkm1Dsim.helpers import convert_cartesian_to_polar, convert_polar_to_cartesian
 # from udkm1Dsim import u
 # u.default_format = '~P'
@@ -10,8 +12,8 @@ import numpy as np
 # from pint.testing import assert_allclose
 
 
-# def test_make_hash_md5():
-#     assert make_hash_md5('test') == '098f6bcd4621d373cade4e832627b4f6'
+def test_make_hash_md5():
+    assert make_hash_md5('test') == '2f4a8dbd4cdc82139c47d0df78b540ac'
 
 
 def test_finderb():
