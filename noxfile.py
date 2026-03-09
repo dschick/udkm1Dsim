3eee0ee106450ee8497c028dd902f68b24c7efd8
Daniel Schick <schick.daniel@gmail.com>
2026-03-09 07:44:33 +0100

    reuse environment in nox testing

--- noxfile.py
+++ noxfile.py
@@ -5,21 +5,21 @@ python_versions = ['3.10', '3.11', '3.12', '3.13']
 
 
 @nox.session(python=python_versions,
-             venv_backend='mamba')
+             venv_backend='mamba', reuse_venv=True)
 def lint(session):
     session.install('flake8')
     session.run('flake8', '.')
 
 
 @nox.session(python=python_versions,
-             venv_backend='mamba')
+             venv_backend='mamba', reuse_venv=True)
 def tests(session):
     session.install('pytest-cov')
     session.install('.')
     session.run('pytest', '--cov=udkm1Dsim', 'test/')
 
 
-@nox.session(python='3.13')
+@nox.session(python='3.13', reuse_venv=True)
 def docs(session):
     session.install('.')
     session.install('-r', './docs/requirements.txt')
