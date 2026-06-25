66515efd026d543c5a993de07481ce25dd12027d
Daniel Schick <schick.daniel@gmail.com>
2026-06-25 23:23:08 +0200

    update docs of coordinate transformations in helpers

--- udkm1Dsim/helpers.py
+++ udkm1Dsim/helpers.py
@@ -215,6 +215,9 @@ def convert_polar_to_cartesian(polar):
     (amplitude), azimuthal, and polar angles of vector field
     :math:`\mathbf{F}`, respectively.
 
+    In case of multidimensional array as input, the relevant vector or
+    field must be contained in its last dimension.
+
     Args:
         polar (ndarray[float]): vector of field to convert.
 
@@ -256,6 +259,9 @@ def convert_cartesian_to_polar(cartesian):
     where :math:`F_r`, :math:`F_{\phi}`, :math:`F_{\gamma}` are the radial
     (amplitude), azimuthal, and polar component of vector field
     :math:`\mathbf{F}`, respectively.
+
+    In case of multidimensional array as input, the relevant vector or
+    field must be contained in its last dimension.
 
     Args:
         cartesian (ndarray[float]): vector of field to convert.
