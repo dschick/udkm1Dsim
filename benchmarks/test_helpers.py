4192184ed1fe4c325ee146bedbc2e02f77382110
Daniel Schick <schick.daniel@gmail.com>
2026-07-20 10:30:30 +0200

    change imports for benchmarks helpers module

--- benchmarks/test_helpers.py
+++ benchmarks/test_helpers.py
@@ -1,21 +1,23 @@
 import pytest
-import udkm1Dsim as ud
+from udkm1Dsim.helpers import m_power_x, m_times_n
+from udkm1Dsim.helpers import finderb, make_hash_md5
+from udkm1Dsim.helpers import convert_cartesian_to_polar, convert_polar_to_cartesian
 import numpy as np
 
 
 @pytest.mark.benchmark
 def test_make_hash_md5():
-    ud.helpers.make_hash_md5([{'item1': np.ones((10, 10, 10))}])
+    make_hash_md5([{'item1': np.ones((10, 10, 10))}])
 
 
 @pytest.mark.benchmark
 def test_m_power_x():
-    ud.helpers.m_power_x(np.random.rand(25, 25, 4, 4), 25)
+    m_power_x(np.random.rand(25, 25, 4, 4), 25)
 
 
 @pytest.mark.benchmark
 def test_m_times_n():
-    ud.helpers.m_times_n(
+    m_times_n(
         np.random.rand(100, 10, 4, 4),
         np.random.rand(100, 10, 4, 4)
         )
@@ -23,14 +25,14 @@ def test_m_times_n():
 
 @pytest.mark.benchmark
 def test_finderb():
-    ud.helpers.finderb(np.random.rand(100), np.linspace(0, 1, 10000))
+    finderb(np.random.rand(100), np.linspace(0, 1, 10000))
 
 
 @pytest.mark.benchmark
 def test_convert_polar_to_cartesian():
-    ud.helpers.convert_polar_to_cartesian(np.random.rand(100, 100, 3))
+    convert_polar_to_cartesian(np.random.rand(100, 100, 3))
 
 
 @pytest.mark.benchmark
 def test_convert_cartesian_to_polar():
-    ud.helpers.convert_cartesian_to_polar(np.random.rand(100, 100, 3))
+    convert_cartesian_to_polar(np.random.rand(100, 100, 3))
