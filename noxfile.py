39175e2927779a50972d59f4e377c712abe83a33
Daniel Schick <schick.daniel@gmail.com>
2026-09-09 22:41:19 +0200

    use uv as backend for venv in nox

--- noxfile.py
+++ noxfile.py
@@ -5,11 +5,11 @@ reuse_venv = False
 
 
 @nox.session(python='3.13',
-             venv_backend='mamba', reuse_venv=reuse_venv)
+             venv_backend='uv', reuse_venv=reuse_venv)
 def lint(session):
     session.install('flake8')
     session.install('nbqa')
-    session.run('flake8', '.')
+    session.run('flake8' '.')
     session.run('nbqa', 'flake8', 'examples/')
 
 
@@ -36,7 +36,7 @@ def lint(session):
         ('3.14', '2.4.1'),
     ],
 )
-@nox.session(venv_backend='mamba', reuse_venv=reuse_venv)
+@nox.session(venv_backend='uv', reuse_venv=reuse_venv)
 def tests(session, numpy):
     session.install(f'numpy=={numpy}')
     session.install('pytest-cov')
@@ -44,14 +44,14 @@ def tests(session, numpy):
     session.run('pytest', '--cov=udkm1Dsim', '--ignore=tests/benchmarks', 'tests/')
 
 
-@nox.session(python='3.13', reuse_venv=reuse_venv)
+@nox.session(venv_backend='uv', python='3.13', reuse_venv=reuse_venv)
 def docs(session):
     session.install('.')
     session.install('-r', './docs/requirements.txt')
     session.run('make', '--directory', './docs', 'html', external=True)
 
 
-@nox.session(venv_backend='mamba', python='3.13', reuse_venv=reuse_venv)
+@nox.session(venv_backend='uv', python='3.13', reuse_venv=reuse_venv)
 def benchmarks(session):
     session.install('.')
     session.install('pytest-codspeed')
